29d25bab804f85f59f920931692da312ba0b829d
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-15 12:25:15 +0800

    Add match.js model

---
+++ backend/src/model/Match.ts
@@ -0,0 +1,9 @@
+export interface Match{
+    id: Number;
+    homeTeam: string;
+    awayTeam: string;
+    homeScore: number | null;
+    awayScore: number | null;
+    status: string;
+    startTime: String;
+}
