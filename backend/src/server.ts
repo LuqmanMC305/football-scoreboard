273ceb869ac7b2a5564840bc2ef9d3c9dac9b416
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-15 12:36:38 +0800

    Removed the temporarily sportmonks api test in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,37 +16,6 @@ app.get("/api/health", (req, res) =>{
     });
 });
 
-// Temporary route to test Sportmonks API
-app.get("/api/fixtures/:id", async (req, res)=>{
-    try{
-        const fixtureId = req.params.id;
-        const token = process.env.SPORTMONKS_API_TOKEN;
-
-        if(!token){
-            return res.status(500).json({
-                error: "Missing API token"
-            });
-        }
-
-        const response = await fetch(
-            `https://api.sportmonks.com/v3/football/fixtures/${fixtureId}?api_token=${token}`
-        );
-
-        const data = await response.json();
-
-        res.status(response.status).json(data);
-
-    } catch (error){
-        console.error(error);
-
-        res.status(500).json({
-            error: "Failed to contact Sportmonks"
-        });
-    }
-});
-
-
-
 // Starts the HTTP server on port 3000
 app.listen(3000, ()=>{
     console.log("Backend running on http://localhost:3000");
