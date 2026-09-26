4ac0f198eb3cbceac4f53322d1770f8c3acbf40f
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-26 21:18:55 +0800

    Readded searchFixture

--- backend/src/providers/SportmonksProvider.ts
+++ backend/src/providers/SportmonksProvider.ts
@@ -2,7 +2,7 @@ import { encode } from "node:punycode";
 
 const BASE_URL = `https://api.sportmonks.com/v3/football`;
 
-export async function getFixture(fixtureId: number){
+export async function getFixture(searchQuery: string){
     const token = process.env.SPORTMONKS_API_TOKEN;
 
     if(!token){
@@ -11,6 +11,25 @@ export async function getFixture(fixtureId: number){
 
     const response = await fetch(
         `${BASE_URL}/fixtures/search/${encodeURIComponent(searchQuery)}?api_token=${token}&include=participants;scores;state`
+    );
+
+    if(!response.ok){
+        throw new Error(`Sportmonks request failed: ${response.status}`);
+    }
+
+    return response.json();
+
+}
+
+export async function searchFixture(fixtureId: string){
+    const token = process.env.SPORTMONKS_API_TOKEN;
+
+    if(!token){
+        throw new Error("Sportmonks API is missing");
+    } 
+
+    const response = await fetch(
+        `${BASE_URL}/fixtures/${fixtureId}?api_token=${token}&include=participants;scores;state`
     );
 
     if(!response.ok){
